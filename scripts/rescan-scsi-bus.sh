openSUSE/sg3_utils

commit e034c1335f9c520570ede4be454ddb0da15088a7

rescan-scsi-bus: improve handling of USB SD card readers; sg_inq: add --quiet option; sg_turs: add --ascq= option; sg_map26: add mapping for bsg devices; recognize nvme devices; sg_pt_linux: support nvme generic device nodes: ng<c>n<n>
git-svn-id: https://svn.bingwo.ca/repos/sg3_utils/trunk@1023 6180dd3e-e324-4e3e-922d-17de1ae2f315

scripts/rescan-scsi-bus.sh
@@ -4,7 +4,7 @@
 # (c) 2006--2022 Hannes Reinecke, GNU GPL v2 or later
 # $Id: rescan-scsi-bus.sh,v 1.57 2012/03/31 14:08:48 garloff Exp $
 
-VERSION="20220930"
+VERSION="20230413"
 SCAN_WILD_CARD=4294967295
 
 TMPLUNINFOFILE="/tmp/rescan-scsi-mpath-info.txt"
@@ -251,7 +251,7 @@ is_removable ()
   if [ -n "$b" ]; then
     echo $(((0x$b & 0x80) != 0))
   else
-    sg_inq /dev/$SGDEV 2>/dev/null | sed -n 's/^.*RMB=\([0-9]*\).*$/\1/p'
+    sg_inq "$sg_len_arg" /dev/$SGDEV 2>/dev/null | sed -n 's/^.*RMB=\([0-9]*\).*$/\1/p'
   fi
 }
 
@@ -273,12 +273,12 @@ testonline ()
   [ ! -x /usr/bin/sg_turs ] && return 0
   sgdevice
   [ -z "$SGDEV" ] && return 0
-  sg_turs /dev/$SGDEV >/dev/null 2>&1
+  sg_turs "$sg_turs_opt" /dev/$SGDEV >/dev/null 2>&1
   RC=$?
 
   # Handle in progress of becoming ready and unit attention
   while [ $RC = 2 -o $RC = 6 ] && [ $ctr -lt $timeout ] ; do
-    if [ $RC = 2 ] && [ "$RMB" != "1" ] && sg_inq /dev/$SGDEV | grep -q -i "PQual=0" ; then
+    if [ $RC = 2 ] && [ "$RMB" != "1" ] && sg_inq "$sg_len_arg" /dev/$SGDEV | grep -q -i "PQual=0" ; then
       echo -n "."
       let LN+=1
       sleep 1
@@ -286,7 +286,7 @@ testonline ()
       sleep 0.02
     fi
     let ctr+=1
-    sg_turs /dev/$SGDEV >/dev/null 2>&1
+    sg_turs "$sg_turs_opt" /dev/$SGDEV >/dev/null 2>&1
     RC=$?
     # Check for removable device; TEST UNIT READY obviously will
     # fail for a removable device with no medium
@@ -468,7 +468,7 @@ udevadm_settle()
     # Loop for up to 60 seconds if sd devices still are settling..
     # This allows us to continue if udev events are stuck on multipaths in recovery mode
     while [ $tmo -gt 0 ] ; do
-      if ! /sbin/udevadm settle --timeout=1 | egrep -q sd[a-z]+ ; then
+      if ! /sbin/udevadm settle --timeout=1 | grep -E -q sd[a-z]+ ; then
         break;
       fi
       let tmo=$tmo-1
@@ -1122,13 +1122,13 @@ findresized()
   if [ -n "$mp_enable" ] && [ -n "$mpaths" ] ; then
     i=0
     for m in $mpaths ; do
-      mpathsizes[$i]="$($MULTIPATH -l "$m" | egrep -o [0-9]+.[0-9]+[KMGT])"
+      mpathsizes[$i]="$($MULTIPATH -l "$m" | grep -E -o [0-9]+.[0-9]+[KMGT])"
       let i=$i+1
     done
     resizempaths
     i=0
     for m in $mpaths ; do
-      mpathsize="$($MULTIPATH -l "$m" | egrep -o [0-9\.]+[KMGT])"
+      mpathsize="$($MULTIPATH -l "$m" | grep -E -o [0-9\.]+[KMGT])"
       echo "$m ${mpathsizes[$i]} => $mpathsize"
       let i=$i+1
     done
@@ -1215,11 +1215,23 @@ if [ -x /usr/bin/sg_inq ] ; then
     let sg_version+=$((100 * sg_ver_maj))
   fi
   sg_version=${sg_version##0.}
+  sg_turs_version=$(sg_turs -V 2>&1 | cut -d " " -f 3)
+  if [ -n "$sg_turs_version" ] ; then
+    sg_tur_maj=${sg_turs_version:0:1}
+    sg_turs_version=${sg_turs_version##?.}
+    let sg_turs_version+=$((100 * sg_tur_maj))
+  fi
+  sg_turs_version=${sg_turs_version##0.}
   #echo "\"$sg_version\""
   if [ -z "$sg_version" ] || [ "$sg_version" -lt 70 ] ; then
     sg_len_arg="-36"
   else
     sg_len_arg="--len=36"
+  fi
+  if [ "$sg_turs_version" -gt 353 ] ; then
+    sg_turs_opt="--ascq=0x3a"
+  else
+    sg_turs_opt=""
   fi
 else
   echo "WARN: /usr/bin/sg_inq not present -- please install sg3_utils"

INSTALL
@@ -20,7 +20,7 @@ will generate the configure script so then './configure; make;
 make install' sequence should succeed. Tarballs of this package should
 include the configure script so the ./autogen.sh step may not be needed.
 Other autotools base packages use the script name 'bootstrap' instead
-of 'autogen.sh', so for compatability a bootstrap script has been added
+of 'autogen.sh', so for compatibility a bootstrap script has been added
 to this package which simply calls ./autogen.sh .
 
 The following more detailed instructions are generic; see the `README'

doc/sg_inq.8
@@ -1,4 +1,4 @@
-.TH SG_INQ "8" "February 2023" "sg3_utils\-1.48" SG3_UTILS
+.TH SG_INQ "8" "April 2023" "sg3_utils\-1.48" SG3_UTILS
 .SH NAME
 sg_inq \- issue SCSI INQUIRY command and/or decode its response
 .SH SYNOPSIS
@@ -8,7 +8,7 @@ sg_inq \- issue SCSI INQUIRY command and/or decode its response
 [\fI\-\-force\fR] [\fI\-\-help\fR] [\fI\-\-hex\fR] [\fI\-\-id\fR]
 [\fI\-\-inhex=FN\fR] [\fI\-\-json[=JO]\fR] [\fI\-\-js\-file=JFN\fR]
 [\fI\-\-len=LEN\fR] [\fI\-\-long\fR] [\fI\-\-maxlen=LEN\fR]
-[\fI\-\-only\fR] [\fI\-\-page=PG\fR] [\fI\-\-raw\fR]
+[\fI\-\-only\fR] [\fI\-\-page=PG\fR]  [\fI\-\-quiet\fR] [\fI\-\-raw\fR]
 [\fI\-\fI\-sinq_inraw=RFN\fR] [\fI\-\-vendor\fR] [\fI\-\-verbose\fR]
 [\fI\-\-version\fR] [\fI\-\-vpd\fR] \fIDEVICE\fR
 .PP
@@ -17,7 +17,7 @@ sg_inq \- issue SCSI INQUIRY command and/or decode its response
 [\fI\-c\fR] [\fI\-cl\fR] [\fI\-d\fR] [\fI\-e\fR] [\fI\-f\fR] [\fI\-h\fR]
 [\fI\-H\fR] [\fI\-i\fR] [\fI\-I=FN\fR] [\fI\-j[=JO]\fR] [\fI\-l=LEN\fR]
 [\fI\-L\fR] [\fI\-m\fR] [\fI\-M\fR] [\fI\-o\fR] [\fI\-p=VPD_PG\fR]
-[\fI\-P\fR] [\fI\-r\fR] [\fI\-s\fR] [\fI\-u\fR] [\fI\-v\fR]
+[\fI\-P\fR] [\fI\-q\fR] [\fI\-r\fR] [\fI\-s\fR] [\fI\-u\fR] [\fI\-v\fR]
 [\fI\-V\fR] [\fI\-x\fR] [\fI\-36\fR] [\fI\-?\fR] \fIDEVICE\fR
 .SH DESCRIPTION
 .\" Add any additional description here
@@ -210,7 +210,8 @@ Without this option an additional SCSI command is sent: a (non\-standard)
 SCSI INQUIRY to fetch the Serial Number VPD page. However the Serial
 Number VPD page is not mandatory (while the Device Identification page is
 mandatory but a billion USB keys ignore that) and may cause nuisance error
-reports.
+reports. In most cases if the \fI\-\-maxlen=LEN\fR option is given and
+\fILEN\fR is greater than 0 then this option is not needed.
 .br
 For NVMe devices only the Identify controller is performed, even if the
 \fIDEVICE\fR includes a namespace identifier. For example in FreeBSD
@@ -230,6 +231,9 @@ can be used to override some guessing logic associated with the
 .br
 If \fIPG\fR is not found in the 'Supported VPD pages' VPD page (0x0) then
 EDOM is returned. To bypass this check use the \fI\-\-force\fR option.
+.TP
+\fB\-q\fR, \fB\-\-quiet\fR
+suppress the amount of decoding and error output.
 .TP
 \fB\-r\fR, \fB\-\-raw\fR
 in the absence of \fI\-\-inhex=FN\fR then the output response is in binary.

doc/sg_map26.8
@@ -1,4 +1,4 @@
-.TH SG_MAP26 "8" "March 2023" "sg3_utils\-1.48" SG3_UTILS
+.TH SG_MAP26 "8" "April 2023" "sg3_utils\-1.48" SG3_UTILS
 .SH NAME
 sg_map26 \- map SCSI generic (sg) device to corresponding device names
 .SH SYNOPSIS
@@ -38,6 +38,9 @@ device nodes in the /dev directory. The lsscsi utility may be used
 instead in many cases, and lsscsi does not require root permission
 as it "data\-mines" the sysfs pseudo file system for the information
 it needs.
+.PP
+For notes on bsg and nvme device nodes see the section on
+BSG and NVME DEVICES below.
 .SH OPTIONS
 Arguments to long options are mandatory for short options as well.
 .TP
@@ -80,6 +83,39 @@ increase the level of verbosity, (i.e. debug output).
 .TP
 \fB\-V\fR, \fB\-\-version\fR
 print the version string and then exit.
+.SH BSG and NVME DEVICES
+The bsg driver (Block Scsi Generic) presents alternate Unix character devices
+of the form: /dev/bsg/<h:c:t:l>. <h:c:t:l> is a 4 element tuple where 'h' is
+the host number, 'c' is the controller number, 't' is the target identifier
+and 'l' is the LUN (Logical Unit Number). Other than the bsg device names,
+each device, once open()\-ed, operates in a similar fashion to a sg device: a
+generic interface at the SCSI command level.
+.PP
+If a bsg device is given to this utility, it will first try to map it to a
+SCSI block device. This will either be of the form /dev/sd<letter_s> for a
+disk or /dev/sr<number> for a BD/DVD or CD drive. If no match is found, then
+a check is made for a SCSI tape drive with a name of the
+form /dev/st<number>. If that fails, the mapping to the corresponding sg
+device is made. If the \fI\-\-result=2\fR option is given, the block device
+and tape davice checks are bypassed and the mapping to the corresponding sg
+device is made.
+.PP
+Note that this utility makes no mapping to a bsg device. That is pretty easy
+to establish using the lsscsi utility as the 4 element <h:c:t:l> tuple is
+shown at the beginning of each line (by default) and it uniquely identifies
+each SCSI device. Each line will also show the primary device name and, if
+the \-\-generic option is given, the sg device node.
+.PP
+Currently NVMe device nodes come in several varieties, block devices of the
+form: /dev/nvme<c>n<n>[p<pn>] where <c> is the controller number (starting
+at 0), <n> is the namespace identifier (starting at 1), and <pn> is the
+partition number (starting at 1). There are two forms of NVMe character
+device nodes: one of the form /dev/nvme<c> and the other of the
+form /dev/ng<c>n<n> where "ng" is newer and termed as 'nvme-generic' in
+the /proc/devices output. This utility identifies but does not map NVMe
+devices. Their naming is more consistent so a utility like this is less
+needed. Udev might be used to remap the kernel's naming scheme for NVMe,
+removing its inherent naming consistency.
 .SH NOTES
 This utility is designed for the Linux 2.6 (and later) kernel series.
 It uses special file major and minor numbers (and whether the special

doc/sg_turs.8
@@ -1,11 +1,12 @@
-.TH SG_TURS "8" "November 2022" "sg3_utils\-1.48" SG3_UTILS
+.TH SG_TURS "8" "April 2023" "sg3_utils\-1.48" SG3_UTILS
 .SH NAME
 sg_turs \- send one or more SCSI TEST UNIT READY commands
 .SH SYNOPSIS
 .B sg_turs
-[\fI\-\-delay=MS\fR] [\fI\-\-help\fR] [\fI\-\-low\fR] [\fI\-\-num=NUM\fR]
-[\fI\-\-number=NUM\fR] [\fI\-\-progress\fR] [\fI\-\-time\fR]
-[\fI\-\-verbose\fR] [\fI\-\-version\fR] \fIDEVICE\fR
+[\fI\-\-ascq=ASC[,ASQ]\fR] [\fI\-\-delay=MS\fR] [\fI\-\-help\fR]
+[\fI\-\-low\fR] [\fI\-\-num=NUM\fR] [\fI\-\-number=NUM\fR]
+[\fI\-\-progress\fR] [\fI\-\-time\fR] [\fI\-\-verbose\fR]
+[\fI\-\-version\fR] \fIDEVICE\fR
 .PP
 .B sg_turs
 [\fI\-d=MS\fR] [\fI\-n=NUM\fR] [\fI\-p\fR]  [\fI\-t\fR] [\fI\-v\fR] [\fI\-V\fR]
@@ -22,6 +23,18 @@ shown first in the synopsis and explained in this section. A later section
 on the old command line syntax outlines the second group of options.
 .SH OPTIONS
 Arguments to long options are mandatory for short options as well.
+.TP
+\fB\-a\fR, \fB\-\-ascq\fR=\fIASC[,ASQ]\fR
+checks the sense data associated with a sense key of NOT READY. If there is
+a match on \fIASC\fR and, if given, with \fIASQ\fR then this utility sets
+its exit status to 36. That is an alternate good code because an exit status
+of 0 would imply the unit is ready.
+.PP
+The reason this option was added is USB card readers (e.g. SD card readers)
+that often yield an additional sense code of 0x3a,0x0 for "no medium present"
+when there is no SD card in the reader. There are variations of additional
+sense code 0x3a so it is best not to give the \fIASQ\fR value so any ascq
+code will match.
 .TP
 \fB\-d\fR, \fB\-\-delay\fR=\fIMS\fR
 this option causes a delay of \fIMS\fR milliseconds to occur before each
@@ -96,11 +109,12 @@ the SCSI REQUEST SENSE command should be used instead.
 .SH EXIT STATUS
 The exit status of sg_turs is 0 when it is successful (e.g. in the case of
 a mechanical disk, it is spun up and ready to accept IO commands). For this
-utility the other exit status values of interest are 2, 12 and 13. 12 is for
+utility the other exit status values of interest are 2, 12, 13 and 36. 12 is
+for the case when the sense key is "not ready" [0x2] and the additional
+sense code ends with "Target port in standby state" [0x4, 0xb]. 13 is for
 the case when the sense key is "not ready" [0x2] and the additional sense
-code ends with "Target port in standby state" [0x4, 0xb]. 13 is for the
-case when the sense key is "not ready" [0x2] and the additional sense code
-is "Target port in unavailable state" [0x4, 0xc]. All other cases when
+code is "Target port in unavailable state" [0x4, 0xc]. The exit status of
+36 is associated with \fI\-\-ascq=ASC[,ASQ]\fR option. All other cases when
 the sense key is "not ready" [0x2] will set the exit status to 2.
 For other exit status values see the sg3_utils(8) man page.
 .SH OLDER COMMAND LINE OPTIONS
@@ -143,7 +157,7 @@ using \fI\-\-old\fR (or \fI\-O\fR) as the first command line option.
 .SH AUTHORS
 Written by D. Gilbert
 .SH COPYRIGHT
-Copyright \(co 2000\-2022 Douglas Gilbert
+Copyright \(co 2000\-2023 Douglas Gilbert
 .br
 This software is distributed under the GPL version 2. There is NO
 warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.

doc/sg_vpd.8
@@ -1,4 +1,4 @@
-.TH SG_VPD "8" "February 2023" "sg3_utils\-1.48" SG3_UTILS
+.TH SG_VPD "8" "April 2023" "sg3_utils\-1.48" SG3_UTILS
 .SH NAME
 sg_vpd \- fetch SCSI VPD page and/or decode its response
 .SH SYNOPSIS
@@ -169,7 +169,7 @@ If \fIPG\fR is not found in the 'Supported VPD pages' VPD page (0x0) then
 EDOM is returned. To bypass this check use the \fI\-\-force\fR option.
 .TP
 \fB\-q\fR, \fB\-\-quiet\fR
-suppress the amount of decoding output.
+suppress the amount of decoding and error output.
 .TP
 \fB\-r\fR, \fB\-\-raw\fR
 if not used with \fI\-\-inhex=FN\fR then output requested VPD page in binary.

lib/sg_pt_linux.c
@@ -7,7 +7,7 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-/* sg_pt_linux version 1.55 20230102 */
+/* sg_pt_linux version 1.56 20230412 */
 
 
 #include <stdio.h>
@@ -50,7 +50,7 @@
 #endif
 
 #ifndef BLOCK_EXT_MAJOR
-#define BLOCK_EXT_MAJOR 259
+#define BLOCK_EXT_MAJOR 259     /* used by NVMe block devices */
 #endif
 
 #define DEF_TIMEOUT 60000       /* 60,000 millisecs (60 seconds) */
@@ -114,6 +114,7 @@ static const char * linux_driver_suggests[] = {
 bool sg_bsg_nvme_char_major_checked = false;
 int sg_bsg_major = 0;
 volatile int sg_nvme_char_major = 0;
+volatile int sg_nvme_gen_char_major = 0;
 
 bool sg_checked_version_num = false;
 int sg_driver_version_num = 0;
@@ -128,13 +129,14 @@ long sg_lin_page_size = 4096;   /* default, overridden with correct value */
 void
 sg_find_bsg_nvme_char_major(int verbose)
 {
-    bool got_one = false;
+    int num_got = 0;
     int n;
-    const char * proc_devices = "/proc/devices";
     char * cp;
     FILE *fp;
     char a[128];
     char b[128];
+    static const int blen = sizeof(b);
+    static const char * proc_devices = "/proc/devices";
 
     sg_lin_page_size = sysconf(_SC_PAGESIZE);
     if (NULL == (fp = fopen(proc_devices, "r"))) {
@@ -142,23 +144,27 @@ sg_find_bsg_nvme_char_major(int verbose)
             pr2ws("fopen %s failed: %s\n", proc_devices, strerror(errno));
         return;
     }
-    while ((cp = fgets(b, sizeof(b), fp))) {
+    while ((cp = fgets(b, blen, fp))) {
         if ((1 == sscanf(b, "%126s", a)) &&
             (0 == memcmp(a, "Character", 9)))
             break;
     }
-    while (cp && (cp = fgets(b, sizeof(b), fp))) {
+    while (cp && (cp = fgets(b, blen, fp))) {
         if (2 == sscanf(b, "%d %126s", &n, a)) {
             if (0 == strcmp("bsg", a)) {
                 sg_bsg_major = n;
-                if (got_one)
+                if (++num_got >= 3)
                     break;
-                got_one = true;
-            } else if (0 == strcmp("nvme", a)) {
-                sg_nvme_char_major = n;
-                if (got_one)
-                    break;
-                got_one = true;
+            } else if (0 == memcmp("nvme", a, 4)) {
+                if (0 == strcmp("nvme-generic", a)) {
+                    sg_nvme_gen_char_major = n;
+                    if (++num_got >= 3)
+                        break;
+                } else if (0 == strcmp("nvme", a)) {
+                    sg_nvme_char_major = n;
+                    if (++num_got >= 3)
+                        break;
+                }
             }
         } else
             break;
@@ -169,8 +175,11 @@ sg_find_bsg_nvme_char_major(int verbose)
                 pr2ws("found sg_bsg_major=%d\n", sg_bsg_major);
             if (sg_nvme_char_major > 0)
                 pr2ws("found sg_nvme_char_major=%d\n", sg_nvme_char_major);
+            if (sg_nvme_gen_char_major > 0)
+                pr2ws("found sg_nvme_gen_char_major=%d\n",
+                      sg_nvme_gen_char_major);
         } else
-            pr2ws("found no bsg not nvme char device in %s\n", proc_devices);
+            pr2ws("found no bsg nor nvme char device in %s\n", proc_devices);
     }
     fclose(fp);
 }
@@ -185,6 +194,7 @@ check_file_type(int dev_fd, struct stat * dev_statp, bool * is_bsg_p,
                 int verbose)
 {
     bool is_nvme = false;
+    bool is_nvme_gen = false;
     bool is_sg = false;
     bool is_bsg = false;
     bool is_char = false;
@@ -206,10 +216,29 @@ check_file_type(int dev_fd, struct stat * dev_statp, bool * is_bsg_p,
             is_char = true;
             if (SCSI_GENERIC_MAJOR == major_num)
                 is_sg = true;
-            else if (sg_bsg_major == major_num)
-                is_bsg = true;
-            else if (sg_nvme_char_major == major_num)
-                is_nvme = true;
+            else {
+                if (! sg_bsg_nvme_char_major_checked) {
+                    sg_bsg_nvme_char_major_checked = true;
+                    sg_find_bsg_nvme_char_major(verbose);
+                }
+                if (sg_bsg_major == major_num)
+                    is_bsg = true;
+                else if (sg_nvme_char_major == major_num)
+                    is_nvme = true;
+                else if (sg_nvme_gen_char_major == major_num) {
+                    is_nvme_gen = true;
+                    nsid = ioctl(dev_fd, NVME_IOCTL_ID, NULL);
+                    if (SG_NVME_BROADCAST_NSID == nsid) {
+                        /* means ioctl error */
+                        os_err = errno;
+                        if (verbose)
+                            pr2ws("%s: ioctl(NVME_IOCTL_ID) failed: %s "
+                                  "(errno=%d) on NVMe generic\n", __func__,
+                                  safe_strerror(os_err), os_err);
+                    } else
+                        os_err = 0;
+                }
+            }
         } else if (S_ISBLK(dev_statp->st_mode)) {
             is_block = true;
             if (BLOCK_EXT_MAJOR == major_num) {
@@ -237,6 +266,8 @@ skip_out:
             pr2ws("sg device\n");
         else if (is_bsg)
             pr2ws("bsg device\n");
+        else if (is_nvme_gen)
+            pr2ws("NVMe generic char device, nsid=%d\n", nsid);
         else if (is_nvme && (0 == nsid))
             pr2ws("NVMe char device\n");
         else if (is_nvme)
@@ -252,7 +283,7 @@ skip_out:
     if (is_bsg_p)
         *is_bsg_p = is_bsg;
     if (is_nvme_p)
-        *is_nvme_p = is_nvme;
+        *is_nvme_p = is_nvme | is_nvme_gen;
     if (nsid_p)
         *nsid_p = nsid;
     if (os_err_p)
@@ -266,8 +297,8 @@ skip_out:
  * device_name. Returns 1 if SCSI generic pass-though device, returns 2 if
  * secondary SCSI pass-through device (in Linux a bsg device); returns 3 is
  * char NVMe device (i.e. no NSID); returns 4 if block NVMe device (includes
- * NSID), or 0 if something else (e.g. ATA block device) or dev_fd < 0.
- * If error, returns negated errno (operating system) value. */
+ * NSID), or 0 if something else (e.g. SCSI or ATA block device) or
+ * dev_fd < 0. If error, returns negated errno (operating system) value. */
 int
 check_pt_file_handle(int dev_fd, const char * device_name, int verbose)
 {
@@ -275,10 +306,6 @@ check_pt_file_handle(int dev_fd, const char * device_name, int verbose)
         pr2ws("%s: dev_fd=%d, device_name: %s\n", __func__, dev_fd,
               device_name);
     /* Linux doesn't need device_name to determine which pass-through */
-    if (! sg_bsg_nvme_char_major_checked) {
-        sg_bsg_nvme_char_major_checked = true;
-        sg_find_bsg_nvme_char_major(verbose);
-    }
     if (dev_fd >= 0) {
         bool is_sg, is_bsg, is_nvme;
         int err;
@@ -343,10 +370,6 @@ scsi_pt_open_flags(const char * device_name, int flags, int verbose)
 {
     int fd;
 
-    if (! sg_bsg_nvme_char_major_checked) {
-        sg_bsg_nvme_char_major_checked = true;
-        sg_find_bsg_nvme_char_major(verbose);
-    }
     if (verbose > 1) {
         pr2ws("open %s with flags=0x%x\n", device_name, flags);
     }
@@ -545,10 +568,6 @@ set_pt_file_handle(struct sg_pt_base * vp, int dev_fd, int verbose)
     struct sg_pt_linux_scsi * ptp = &vp->impl;
     struct stat a_stat;
 
-    if (! sg_bsg_nvme_char_major_checked) {
-        sg_bsg_nvme_char_major_checked = true;
-        sg_find_bsg_nvme_char_major(verbose);
-    }
     ptp->dev_fd = dev_fd;
     if (dev_fd >= 0) {
         ptp->is_sg = check_file_type(dev_fd, &a_stat, &ptp->is_bsg,
@@ -1128,10 +1147,6 @@ do_scsi_pt(struct sg_pt_base * vp, int fd, int time_secs, int verbose)
     struct sg_pt_linux_scsi * ptp = &vp->impl;
     bool have_checked_for_type = (ptp->dev_fd >= 0);
 
-    if (! sg_bsg_nvme_char_major_checked) {
-        sg_bsg_nvme_char_major_checked = true;
-        sg_find_bsg_nvme_char_major(verbose);
-    }
     if (ptp->in_err) {
         if (verbose)
             pr2ws("Replicated or unused set_scsi_pt... functions\n");

src/sg_decode_sense.c
@@ -30,7 +30,7 @@
 #include "sg_unaligned.h"
 
 
-static const char * version_str = "1.37 20230326";
+static const char * version_str = "1.38 20230411";
 
 #define MY_NAME "sg_decode_sense"
 
@@ -52,6 +52,8 @@ static struct option long_options[] = {
     {"json", optional_argument, 0, 'j'},
     {"js-file", required_argument, 0, 'J'},
     {"js_file", required_argument, 0, 'J'},
+    {"list-err", no_argument, 0, 'l'},
+    {"list_err", no_argument, 0, 'l'},
     {"nodecode", no_argument, 0, 'N'},
     {"nospace", no_argument, 0, 'n'},
     {"status", required_argument, 0, 's'},
@@ -66,9 +68,10 @@ struct opts_t {
     bool do_cdb;
     bool do_help;
     bool do_json;
+    bool do_list_err;
+    bool do_status;
     bool no_decode;
     bool no_space;
-    bool do_status;
     bool verbose_given;
     bool version_given;
     bool err_given;
@@ -76,6 +79,7 @@ struct opts_t {
     bool ignore_first;
     const char * fname;
     int es_val;
+    int es_up_val;
     int hex_count;
     int sense_len;
     int sstatus;
@@ -94,15 +98,15 @@ static char concat_buff[1024];
 static void
 usage()
 {
-  pr2serr("Usage: sg_decode_sense [--binary=BFN] [--cdb] [--err=ES] "
+  pr2serr("Usage: sg_decode_sense [--binary=BFN] [--cdb] [--err=ES[,LES]] "
           "[--file=HFN]\n"
           "                       [--help] [--hex] [--inhex=HFN] "
           "[--ignore-first]\n"
           "                       [--json[=JO]] [--js_file=JFN] "
-          "[--nodecode]\n"
-          "                       [--nospace] [--status=SS] "
-          "[--verbose] [--version]\n"
-          "                       [--write=WFN] H1 H2 H3 ...\n"
+          "[--list-err]\n"
+          "                       [--nodecode] [--nospace] [--status=SS] "
+          "[--verbose]\n"
+          "                       [--version] [--write=WFN] H1 H2 H3 ...\n"
           "  where:\n"
           "    --binary=BFN|-b BFN    BFN is a file name to read sense "
           "data in\n"
@@ -112,6 +116,8 @@ usage()
           "sense data\n"
           "    --err=ES|-e ES        ES is Exit Status from utility in this "
           "package\n"
+          "    --err=ES,LES|-e ES,LES    ES,LES is a range of exit status "
+	  "values\n"
           "    --file=HFN|-f HFN     HFN is a file name from which to read "
           "sense data\n"
           "                          in ASCII hexadecimal. Interpret '-' "
@@ -137,6 +143,8 @@ usage()
           "output is\n"
           "                            written (def: stdout); truncates "
           "then writes\n"
+          "    --list-err|-l         list all error codes and meanings for "
+          "sg3_utils\n"
           "    --nodecode|-N         do not decode, input hex or binary may "
           "be\n"
           "                          unrelated to SCSI sense or CDB formats\n"
@@ -166,10 +174,11 @@ parse_cmd_line(struct opts_t *op, int argc, char *argv[])
     unsigned int ui;
     long val;
     char * avp;
-    char *endptr;
+    const char * ccp;
+    char * endptr;
 
     while (1) {
-        c = getopt_long(argc, argv, "b:ce:f:hHi:Ij::J:nNs:vVw:",
+        c = getopt_long(argc, argv, "b:ce:f:hHi:Ij::J:lnNs:vVw:",
                         long_options, NULL);
         if (c == -1)
             break;
@@ -188,13 +197,23 @@ parse_cmd_line(struct opts_t *op, int argc, char *argv[])
             op->do_cdb = true;
             break;
         case 'e':
-            n = sg_get_num(optarg);
+	    ccp = strchr(optarg, ',');
+            n = sg_get_num_nomult(optarg);
             if ((n < 0) || (n > 255)) {
                 pr2serr("--err= expected number from 0 to 255 inclusive\n");
                 return SG_LIB_SYNTAX_ERROR;
             }
             op->err_given = true;
             op->es_val = n;
+	    if (ccp) {
+		n = sg_get_num_nomult(ccp + 1);
+                if ((n < 1) || (n > 255)) {
+                    pr2serr("--err=<l>,<u> expected number from 1 to 255 "
+			    "inclusive\n");
+                    return SG_LIB_SYNTAX_ERROR;
+                }
+                op->es_up_val = n;
+	    }
             break;
         case 'f':
             if (op->fname) {
@@ -231,6 +250,9 @@ parse_cmd_line(struct opts_t *op, int argc, char *argv[])
        case 'J':
             op->do_json = true;
             op->js_file = optarg;
+            break;
+        case 'l':
+            op->do_list_err = true;
             break;
         case 'n':
             op->no_space = true;
@@ -306,6 +328,38 @@ parse_cmd_line(struct opts_t *op, int argc, char *argv[])
     }
 the_end:
     return 0;
+}
+
+static void
+enumerate_err_codes(const struct opts_t *op)
+{
+    int k = 0;
+    int last = 127;
+    char b[168];
+    static const int blen = sizeof(b);
+
+    if (op->err_given && (! op->do_list_err)) {
+        if (! sg_exit2str(op->es_val, op->verbose > 1, blen, b))
+            snprintf(b, blen, "Unable to decode exit status %d", op->es_val);
+        if (1 & op->verbose) /* odd values of verbose print to stderr */
+            pr2serr("%s\n", b);
+        else    /* even values of verbose (including not given) to stdout */
+            printf("%s\n", b);
+        return;
+    }
+    if (op->err_given) {
+	k = op->es_val;
+	if (op->es_up_val > 0)
+	    last = op->es_up_val;
+    }
+    for ( ; k <= last; ++k) {
+        if (sg_exit2str(k, op->verbose > 1, blen, b)) {
+            if (1 & op->verbose) /* odd values of verbose print to stderr */
+                pr2serr("%d: %s\n", k, b);
+            else    /* even values of verbose (incl. not given) to stdout */
+                printf("%d: %s\n", k, b);
+        }
+    }
 }
 
 /* Keep this format (e.g. 0xff,0x12,...) for backward compatibility */
@@ -393,6 +447,10 @@ main(int argc, char *argv[])
         usage();
         goto clean_op;
     }
+    if (op->do_list_err || op->err_given) {
+        enumerate_err_codes(op);
+        goto clean_op;
+    }
     jsp = &op->json_st;
     if (op->do_json) {
        if (! sgj_init_state(jsp, op->json_arg)) {
@@ -412,18 +470,6 @@ main(int argc, char *argv[])
     }
     as_json = jsp->pr_as_json;
 
-    if (op->err_given) {
-        char d[128];
-        const int dlen = sizeof(d);
-
-        if (! sg_exit2str(op->es_val, op->verbose > 1, dlen, d))
-            snprintf(d, dlen, "Unable to decode exit status %d", op->es_val);
-        if (1 & op->verbose) /* odd values of verbose print to stderr */
-            pr2serr("%s\n", d);
-        else    /* even values of verbose (including not given) to stdout */
-            printf("%s\n", d);
-        goto fini;
-    }
 
     if (op->do_status) {
         sg_get_scsi_status_str(op->sstatus, blen, b);

commit 0bde74b4feeddc66ee993558fb017363a5e41921

rescan-scsi-bus.sh: fix for github.com/doug-gilbert/sg3_utils/issues/46
The uses of

   sg_turs "$sg_turs_opt" /dev/$SGDEV >/dev/null 2>&1

are incorrect if there are no options, as it passes the empty string as the
first argument to sg_turs which treats it as the device positional
argument. The quotes should be removed (though I don't know if that affects
the case where there is an argument).

git-svn-id: https://svn.bingwo.ca/repos/sg3_utils/trunk@1076 6180dd3e-e324-4e3e-922d-17de1ae2f315

In the worst case, this may lead to file systems going read only because
a SCSI device is erroneously considered offline.

Fixes: e034c13 ("rescan-scsi-bus: improve handling of USB SD card readers")

scripts/rescan-scsi-bus.sh
@@ -1240,7 +1240,9 @@ if [ -x /usr/bin/sg_inq ] ; then
   if [ "$sg_turs_version" -gt 353 ] ; then
     sg_turs_opt="--ascq=0x3a"
   else
-    sg_turs_opt=""
+    # Not really interested in timing a TUR but an empty string such as
+    # "" gets interpreted on the command line as a positional parameter.
+    sg_turs_opt="-t"
   fi
 else
   echo "WARN: /usr/bin/sg_inq not present -- please install sg3_utils"